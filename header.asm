79eef14a5c60bfbb0f10d85d68b6017e7790ed30
TempoQuill <jicrapo@gmail.com>
2022-01-31 21:07:16 -0500

    Fix headers

--- header.asm
+++ header.asm
@@ -2,9 +2,9 @@
 ; MAP: MMC5
 ; PRG: 1024K
 ; CHR: 1024K
-; RAM: 128K + 2K internal
-; TOTAL: 2178K
-	.db $4e, $45, $53, $1a ; NES + end of file command
+; RAM: 128K + 2K internal + 1K Chip
+; TOTAL: 2179K
+	.db "NES", $1a ; NES + end of file command
 	.db $40 ; 1 megabyte of PRG data
 	.db $80 ; 1 megabyte of CHR data
 	.db MMC5 & %00001111 << 4 | IGNORE_MIRRORING | BATTERY_RAM
--- nsf-header.asm
+++ nsf-header.asm
@@ -6,15 +6,15 @@
 	.dw LOAD
 	.dw INIT
 	.dw PLAY
-	ascii "VULPREICH"
+	.db "VULPREICH"
 	.dsb $17, $0
-	ascii "TEMPO QUILL"
+	.db "TEMPO QUILL"
 	.dsb $15, $0
-	ascii "2022 Free use active postsale"
+	.db "2022 Free use active postsale"
 	.dsb $3, $0
 	.dw $411a ; NTSC
 	.db PRG_Audio, PRG_Audio + 1, PRG_Music0, PRG_Music0 + 1, PRG_DPCM0, PRG_DPCM0 + 1, PRG_Home + 7, PRG_Home
 	.dw $4e20 ; PAL, unused
 	.db 0 ; this is an NTSC file
 	.db 0 ; no extra chip (North American settings)
-	.dsb 3, 0 ; proceeding data is program data
+	.dsb 4, 0 ; proceeding data is program data
