4d4fbc7806c6c41c4146d1ea23c226aed81adac8
TempoQuill <jicrapo@gmail.com>
2022-02-01 14:27:58 -0500

    Complete main title screen

--- src/engine/init.asm
+++ src/engine/init.asm
@@ -56,6 +56,9 @@ IntroPals:
 IntroSequence:
 	JSR InspiredScreen
 	JSR TitleScreen
+	LDA #1
+	JSR DelayFrame_s_
+	JSR RunTitleScreen
 	LDA zPPUCtrlMirror
 	RSB PPU_OBJECT_RESOLUTION ; 8x8
 	STA zPPUCtrlMirror
@@ -172,8 +175,92 @@ TitleScreen:
 	LDA iPals
 	SSB PAL_FADE_F
 	STA iPals
-	SSB PAL_FADE_DIR_F
+	RTS
+
+RunTitleScreen:
+	; check for controller 1 input
+	JSR Intro_CheckInput
+	BCC @Quit
+	LDA @DW, Y
+	INY
+	STA zAuxAddresses + 6
+	LDA @DW, Y
+	STA zAuxAddresses + 7
+	JMP (zAuxAddresses + 6)
+@Quit:
+	RTS
+
+@DW:
+	.dw @Press_A_Start
+	.dw @Press_B
+	.dw @Press_Up_Down
+
+@Press_A_Start:
+	LDA zInputBottleNeck
+	AND #1 << START_BUTTON
+	BNE @CheckSelect
+@Normal:
+	LDA #TITLESCREENOPTION_MAIN_MENU
+	STA zTitleScreenOption
+	JSR ClearOAM
+	LDY #MUSIC_NONE
+	JSR PlayMusic
+	LDY #SFX_SELECT_1
+	JSR PlaySFX
+	; fade out palettes
+	LDA iPals
+	ORA #1 << PAL_FADE_F | 1 << PAL_FADE_DIR_F
+	STA iPals
+	RSB PAL_FADE_DIR_F ; wait $8f frames (2.38 seconds)
+	JMP DelayFrame_s_
+
+@CheckSelect:
+	LDA zInputBottleNeck
+	AND #1 << SELECT_BUTTON
+	BEQ @Normal
+	LDA #TITLESCREENOPTION_DELETE_SAVE_FILE
+	STA zTitleScreenOption
+	RTS
+
+@Press_B:
+	LDY #MUSIC_NONE
+	JSR PlayMusic
+	JSR ClearOAM
+	LDA iPals
+	ORA #1 << PAL_FADE_F | 1 << PAL_FADE_DIR_F
+	STA iPals
+	RSB PAL_FADE_DIR_F ; wait $8f frames (2.38 seconds)
 	JSR DelayFrame_s_
+	RTS
+
+@Press_Up_Down:
+	LDY #SFX_CURSOR_1
+	JSR PlaySFX
+	RTS
+
+Intro_CheckInput:
+; c = necesary input (a, b, start, up, down)
+; y = pointer offset (0 for a/start, 2 for b, 4 for up/down)
+	LDY #0
+	LDA zInputBottleNeck
+	ASL A
+	BCS @A_Start
+	ASL A
+	BCS @B
+	ASL A
+	BCS @A_Start
+	ASL A
+	ASL A
+	BCS @Up_Down
+	BMI @Up_Down
+	RTS
+@Up_Down:
+	INY
+	INY
+@B:
+	INY
+	INY
+@A_Start:
 	RTS
 
 LogoData:
