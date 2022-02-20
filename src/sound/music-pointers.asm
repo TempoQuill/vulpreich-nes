4eac5d70bc834749ab60e756a1aff389b9c89092
TempoQuill <jicrapo@gmail.com>
2022-02-20 16:24:27 -0500

    Correct NSF Bank numbers

--- src/sound/music-pointers.asm
+++ src/sound/music-pointers.asm
@@ -1,4 +1,8 @@
 Music:
-	dba PRG_Audio,  Music_None
-	dba PRG_Music0, Music_TitleScreen
-	dba PRG_Music0, Music_Journey
+IFDEF NSF_FILE
+	dba PRG_Audio + 1, Music_None
+ELSE
+	dba PRG_Audio,     Music_None
+ENDIF
+	dba PRG_Music0,    Music_TitleScreen
+	dba PRG_Music0,    Music_Journey
--- src/sound/sfx-pointers.asm
+++ src/sound/sfx-pointers.asm
@@ -1,4 +1,35 @@
 SFX:
+IFDEF NSF_FILE
+	dba PRG_Audio + 1, SFX_FoxExclamation
+	dba PRG_Audio + 1, SFX_CatExclamation
+	dba PRG_Audio + 1, SFX_DogExclamation
+	dba PRG_Audio + 1, SFX_FoxCursor
+	dba PRG_Audio + 1, SFX_CatCursor
+	dba PRG_Audio + 1, SFX_DogCursor
+	dba PRG_Audio + 1, SFX_FoxSelect
+	dba PRG_Audio + 1, SFX_CatSelect
+	dba PRG_Audio + 1, SFX_DogSelect
+	dba PRG_Audio + 1, SFX_IggyVoice0
+	dba PRG_Audio + 1, SFX_JuneVoice0
+	dba PRG_Audio + 1, SFX_OtisVoice0
+	dba PRG_Audio + 1, SFX_IggyVoice1
+	dba PRG_Audio + 1, SFX_JuneVoice1
+	dba PRG_Audio + 1, SFX_OtisVoice1
+	dba PRG_Audio + 1, SFX_IggyVoice2
+	dba PRG_Audio + 1, SFX_JuneVoice2
+	dba PRG_Audio + 1, SFX_OtisVoice2
+	dba PRG_Audio + 1, SFX_IggyVoice3
+	dba PRG_Audio + 1, SFX_JuneVoice3
+	dba PRG_Audio + 1, SFX_OtisVoice3
+	dba PRG_Audio + 1, SFX_IggyVoice4
+	dba PRG_Audio + 1, SFX_JuneVoice4
+	dba PRG_Audio + 1, SFX_OtisVoice4
+	dba PRG_Audio + 1, SFX_DoorClick
+	dba PRG_Audio + 1, SFX_DoorShut
+	dba PRG_Audio + 1, SFX_DoorSlam
+	dba PRG_Audio + 1, SFX_Sink
+	dba PRG_Audio + 1, SFX_ObjectiveComplete
+ELSE
 	dba PRG_Audio,  SFX_FoxExclamation
 	dba PRG_Audio,  SFX_CatExclamation
 	dba PRG_Audio,  SFX_DogExclamation
@@ -28,3 +59,4 @@ SFX:
 	dba PRG_Audio,  SFX_DoorSlam
 	dba PRG_Audio,  SFX_Sink
 	dba PRG_Audio,  SFX_ObjectiveComplete
+ENDIF
