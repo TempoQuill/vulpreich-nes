24894cea451d4518f4782e303107fba2eb8670a5
TempoQuill <jicrapo@gmail.com>
2022-02-20 14:00:58 -0500

    Correct assembler PRG math

--- header.asm
+++ header.asm
@@ -34,7 +34,7 @@ ELSE
 	.db "2022 Free to use when sales end"
 	.db 0
 	.dw $411a ; NTSC
-	.db PRG_Audio, PRG_Audio + 1, PRG_Music0, PRG_Music0 + 1, PRG_DPCM0, PRG_DPCM0 + 1, PRG_Home, PRG_Home + 7
+	.db PRG_Audio, PRG_Audio + 1, PRG_Music0, PRG_Music0 + 1, PRG_DPCM0, PRG_DPCM0 + 1, PRG_Home, PRG_Home + 1
 	.dw $4e20 ; PAL, unused
 	.db 0 ; this is an NTSC file
 	.db 0 ; no extra chip (North American settings)
