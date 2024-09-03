d86e0edd9c50f8afe52ad8cda3367ba2eb033a12
Tempo Quill <jicrapo@gmail.com>
2024-09-02 20:57:45 -0400

    Add a starting offset check

--- tools/bmpcheck.py
+++ tools/bmpcheck.py
@@ -1,3 +1,4 @@
+target_start_offs = 154
 target_bit_depth = 4
 with open("input.bmp", "rb") as binary_file:
 	# first check the bit depth
@@ -15,3 +16,19 @@ with open("input.bmp", "rb") as binary_file:
 			print("Bit depth match:", depth_check)
 		except Exception as e:
 			print("ERROR: Bit depth needs to be 4BPP. Index your colors down to 4 entries", e)
+
+	# Now check for a start offset of $9a
+	binary_file.seek(10, 0)
+	start_offset = binary_file.read(1)
+
+	# Look at one of the bitmaps on HxD, the header byte was 154 bytes long under 4BPP mode. It's not
+	# excatly know why 154 specifically, though it could be just 64 bytes of whitespace starting after
+	# the "BGRs" string followed by the four colors needed to convert the bitmap to the 2BPP CHR ROM
+	def doth_offset_match(start_offset, target_start_offs)
+		return start_offset == target_start_offs
+		try:
+			offset_check = doth_offset_match(target_start_offs, start_offset)
+			print("Target start offset:", target_start_offs)
+			print("Start offset match:", offset_check)
+		except Exception as e:
+			print("ERROR: Starting offset must be $9a. Image may have more than four colors.", e)
